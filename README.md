35c62033607212f7e01be997d0980f19165e127f
Chris <djchrismarc@gmail.com>
2018-11-12 09:04:49 +0000

    header adjust 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mchf_ui_boot
+
 mcHF rev 0.8 UI board bootloader
+
--- startup/main.c
+++ startup/main.c
@@ -1,19 +1,19 @@
 /************************************************************************************
-**                                                                                 **
-**                             mcHF Pro QRP Transceiver                            **
-**                         Krassi Atanassov - M0NKA 2012-2018                      **
-**                            mail: djchrismarc@gmail.com                          **
-**                                 twitter: @M0NKA_                                **
-**---------------------------------------------------------------------------------**
-**                                                                                 **
-**  File name:                                                                     **
-**  Description:                                                                   **
-**  Last Modified:                                                                 **
-**  Licence:																	   **
-**  			The mcHF project is released for radio amateurs experimentation,   **
-**  			non-commercial use only! All source files under GPL-3.0, unless    **
-**  			third party drivers specifies otherwise. Thank you!         	   **
-**  																			   **
+**                                                                                 	**
+**                             mcHF Pro QRP Transceiver                            	**
+**                         Krassi Atanassov - M0NKA 2012-2018                      	**
+**                            mail: djchrismarc@gmail.com                         	**
+**                                 twitter: @M0NKA_                                	**
+**---------------------------------------------------------------------------------	**
+**                                                                                 	**
+**  File name:                                                                     	**
+**  Description:                                                                   	**
+**  Last Modified:                                                                 	**
+**  Licence:																	   	**
+**  			The mcHF project is released for radio amateurs experimentation,   	**
+**  			non-commercial use only! All source files under GPL-3.0, unless    	**
+**  			third party drivers specifies otherwise. Thank you!         	   	**
+**  																			   	**
 ************************************************************************************/
 
 #include "main.h"
